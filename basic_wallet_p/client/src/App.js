8f4f19f41300a2bbc8503d60623a1843aa50e24a
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-12 13:17:51 +0000

    Added username on change to react app

--- basic_wallet_p/client/src/App.js
+++ basic_wallet_p/client/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const server = 'http://localhost:5000'
+    const [lastUsername, setLastUsername] = useState('melqui')
+    const [username, setUsername] = useState('melqui')
+    const onChangeUsername = (e) => {
+        const { value } = e.target;
+        setUsername(value)
+    }
+    const onChangeUsernameSubmit = (e) => {
+        e.preventDefault();
+        axios.post(`${server}/changeUsername`, { lastUsername, username }).then(res => {
+            console.log(res)
+            setLastUsername(username)
+        }).catch(err => console.error(err));
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={onChangeUsernameSubmit}>
+                <label htmlFor="username">User name:</label>
+                <input id="username" type="text" value={username} onChange={onChangeUsername} />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
