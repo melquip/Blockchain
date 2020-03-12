965c2193a7e7a1c5fd3ca2035b6ed6243f196b31
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-12 13:30:02 +0000

    Added balance to react app

--- basic_wallet_p/client/src/App.js
+++ basic_wallet_p/client/src/App.js
@@ -5,10 +5,20 @@ function App() {
     const server = 'http://localhost:5000'
     const [lastUsername, setLastUsername] = useState('melqui')
     const [username, setUsername] = useState('melqui')
+    const [balance, setBalance] = useState(0)
+
+    useEffect(() => {
+        axios.post(`${server}/getUserBalance`, { 'username': lastUsername }).then(res => {
+            console.log('getUserBalance', res)
+            setBalance(res.data.balance)
+        }).catch(err => console.error(err));
+    }, [])
+
     const onChangeUsername = (e) => {
         const { value } = e.target;
         setUsername(value)
     }
+
     const onChangeUsernameSubmit = (e) => {
         e.preventDefault();
         axios.post(`${server}/changeUsername`, { lastUsername, username }).then(res => {
@@ -19,6 +29,7 @@ function App() {
 
     return (
         <div className="App">
+            <h3>{lastUsername}'s balance: {balance} Coins</h3>
             <form onSubmit={onChangeUsernameSubmit}>
                 <label htmlFor="username">User name:</label>
                 <input id="username" type="text" value={username} onChange={onChangeUsername} />
